6dc8f937691deb006b15704ee045e4ab08bcc18c
liulin95 <2112326545@qq.com>
2020-12-08 18:44:05 +0800

    init

---
+++ console/src/main/java/com/ll/gateway/console/ConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gateway.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleApplication.class, args);
+    }
+
+}
